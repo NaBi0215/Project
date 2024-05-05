8aa695819b27097ea16a1d83d9d621fb3f47fec2
NaBi0215 <cocoshzmiki@gmail.com>
2024-05-05 20:51:05 +0800

    20240505_05

--- project_index.js
+++ project_index.js
@@ -5,7 +5,7 @@ $(function () {
 
 
     for(let i=0; i<imgCount; i++){
-        $('#contentButton').append('<li></li>')
+    $('#contentButton').append('<li></li>')
     }
     $('#contentButton li:first').addClass('clicked')
 
@@ -17,7 +17,6 @@ $(function () {
     let timer = setInterval(moveToNext,5000)
 
     $('#contentButton li').click(function(){
-        // alert($(this).index())
 
         clearInterval(timer);
         
@@ -52,3 +51,63 @@ function moveToNext(){
 
     }
 });
+
+// $(function () {
+//     let divWidth;
+//     let imgCount;
+//     let index = 0;
+//     let timer;
+
+//     function initializeCarousel() {
+//         divWidth = $('#sliderBoard').width();
+//         imgCount = $('#content li').length;
+
+//         // 重新设置 li 的宽度和 ul 的宽度
+//         $('#content li').width(divWidth);
+//         $('#content').width(divWidth * imgCount);
+
+//         // 初始化第一个按钮为选中状态
+//         $('#contentButton li:first').addClass('clicked');
+
+//         // 初始化轮播定时器
+//         timer = setInterval(moveToNext, 5000);
+//     }
+
+//     initializeCarousel(); // 页面加载时初始化轮播
+
+//     // 监听窗口大小变化
+//     $(window).resize(function() {
+//         clearInterval(timer); // 清除定时器
+//         initializeCarousel(); // 重新初始化轮播
+//     });
+
+//     // 点击按钮切换图片
+//     $('#contentButton li').click(function(){
+//         clearInterval(timer);
+//         index = $(this).index();
+
+//         $('#content').animate({
+//             left: divWidth * index * -1,
+//         })
+
+//         $(this).addClass('clicked')
+//         $('#contentButton li').not(this).removeClass('clicked')
+
+//         timer = setInterval(moveToNext, 5000);
+//     })
+
+//     function moveToNext(){
+//         if(index < imgCount - 1){
+//             index++
+//         } else {
+//             index = 0;
+//         }
+
+//         $('#content').animate({
+//             left: divWidth * index * -1,
+//         })
+
+//         $(`#contentButton li:eq(${index})`).addClass('clicked')
+//         $('#contentButton li').not(`:eq(${index})`).removeClass('clicked')
+//     }
+// });
